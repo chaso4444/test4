81a0a4a76b9d0b591a9c799c090d7d6a2614b2b6
澤野 悠一郎 <ys.app1e.sy@gmail.com>
2021-02-06 16:02:19 +0900

    first commit

---
+++ DjangoWebProject3/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ys14787\source\repos\DjangoWebProject3\DjangoWebProject3\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
